76dd3a8327420be2c5a7779740502165aec83ba8
ndholovu <ndholovu@gmail.com>
2025-09-20 14:14:14 +0900

    20250920

---
+++ main01.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return {"item_name": item.name, "price": item.price}
